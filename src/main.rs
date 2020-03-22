b96360ac751311360eb7bd1ffb96780a65f8897b
Ameer Hamza <ameernormie@gmail.com>
2020-03-22 16:14:52 +0500

    feat: improve guessing game by using custom type

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,24 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100 {}", value);
+        }
+
+        Guess { value }
+    }
+
+    fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
 
@@ -16,15 +34,18 @@ fn main() {
             .expect("Failed to read line");
         /* we want to convert the String the program reads as input into a real number
         type so we can compare it numerically to the secret number.
-        Rust allows us to shadow the previous value of guess with a new one.
+        Rust allows us to shadow the previous value of gue  ss with a new one.
         This feature is often used in situations in which you want to convert a value from one type to another type.
         The parse method on strings parses a string into some kind of number.
         The parse method returns a Result type. If parse returns an Err Result variant because it couldn’t
         create a number from the string, the expect call will crash the game and print the message we give it.
         */
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => Guess::new(num).value(),
+            Err(_) => {
+                println!("Please input a number");
+                continue;
+            }
         };
         println!("You guessed: {}", guess);
         match guess.cmp(&secret_number) {
